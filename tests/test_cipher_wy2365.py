7bd93419d7555c83e8f204a13757dfc92947b6d9
Weiran Yu <weiranyu@hotmail.com>
2020-11-14 20:51:52 +0800

    Update test_cipher_wy2365.py

--- tests/test_cipher_wy2365.py
+++ tests/test_cipher_wy2365.py
@@ -1,5 +1,12 @@
 from cipher_wy2365 import __version__
 from cipher_wy2365 import cipher_wy2365
+import pytest
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_cipher_with_word():
+    example = 'Columbia'
+    expected = 'Dpmvncjb'
+    actual = cipher_wy2365.cipher(example, 1)
+    assert expected == actual, 'Should be True'
